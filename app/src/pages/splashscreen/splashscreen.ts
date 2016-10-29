f5c7a8cff1273b61b8ccf0aee519e6c428728949
chlablak <chlablak@gmail.com>
2016-10-29 12:29:00 +0200

    mobile-ui/splashscreen done

--- app/src/pages/splashscreen/splashscreen.ts
+++ app/src/pages/splashscreen/splashscreen.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Splashscreen page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
-  selector: 'page-splashscreen',
   templateUrl: 'splashscreen.html'
 })
 export class Splashscreen {
 
   constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello Splashscreen Page');
-  }
 
 }
