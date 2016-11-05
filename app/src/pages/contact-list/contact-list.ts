ebabddd64d055353b24bc839ae8e8a822fab10d9
Ornidon <thib.loiseau@gmail.com>
2016-11-05 17:57:41 +0100

    Adding fabs (bad icon and positioning)

--- app/src/pages/contact-list/contact-list.ts
+++ app/src/pages/contact-list/contact-list.ts
@@ -42,9 +42,17 @@ export class ContactList {
     console.log('Hello ContactList Page');
   }
 
+  goToAddContact() {
+
+  }
+
+  goToWaitingForParticipant() {
+
+  }
+
 }
 
 class Person {
   name: string;
-  constructor(theName: string) { this.name = theName; }
+  constructor(name: string) { this.name = name; }
 }
