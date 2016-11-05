34ab4f8b4ae196e14f3632ce64bf7eb0aeb47508
Ornidon <thib.loiseau@gmail.com>
2016-11-05 17:34:05 +0100

    Adding items in contact list and search-bar

--- app/src/pages/contact-list/contact-list.ts
+++ app/src/pages/contact-list/contact-list.ts
@@ -3,15 +3,48 @@ import { NavController } from 'ionic-angular';
 
 
 @Component({
-  selector: 'page-contact-list',
   templateUrl: 'contact-list.html'
 })
 export class ContactList {
 
-  constructor(public navCtrl: NavController) {}
+
+  private items: Person[];
+
+
+  constructor(public navCtrl: NavController) {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      new Person('Sauron'),
+      new Person('Frodon'),
+      new Person('Gandalf')
+    ]
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 
   ionViewDidLoad() {
     console.log('Hello ContactList Page');
   }
 
+}
+
+class Person {
+  name: string;
+  constructor(theName: string) { this.name = theName; }
 }
