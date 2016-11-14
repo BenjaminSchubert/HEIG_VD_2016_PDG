d2e5ce1d54fd6f41596e59e707ce00ab539cb53e
Ornidon <thib.loiseau@gmail.com>
2016-11-14 16:52:55 +0100

    Edit profile part 1, first try, functionnal

--- app/src/pages/edit-profile/edit-profile.ts
+++ app/src/pages/edit-profile/edit-profile.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Validators, FormBuilder } from '@angular/forms';
+
+import { RadyModule } from '../../lib/validators';
 
 @Component({
-  selector: 'page-edit-profile',
   templateUrl: 'edit-profile.html'
 })
 export class EditProfile {
 
-  constructor(public navCtrl: NavController) {}
+  form;
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {}
 
   ionViewDidLoad() {
-    console.log('Hello EditProfile Page');
+  	// create the form with validation
+  	this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, RadyModule.Validators.email])],
+      phone: ['', RadyModule.Validators.phone],
+      password: ['', Validators.required],
+      passwordConfirmation: ['', Validators.required]
+  	}, { validator: RadyModule.Validators.areEqual(['password', 'passwordConfirmation']) });
+  }
+
+  save(){
+    //TODO : save
   }
 
 }
