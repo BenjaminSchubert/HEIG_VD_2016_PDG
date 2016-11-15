fcaee08148033be223ba1fcc35a4ce2bc5d2a7fd
Ornidon <thib.loiseau@gmail.com>
2016-11-15 14:53:41 +0100

    Updating Edit profile from register and changing way to switch from tabs

--- app/src/pages/edit-profile/edit-profile.ts
+++ app/src/pages/edit-profile/edit-profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Tabs } from 'ionic-angular';
 import { Validators, FormBuilder } from '@angular/forms';
+
 
 import { RadyModule } from '../../lib/validators';
 
@@ -18,22 +19,35 @@ export class EditProfile {
     //TODO : GET CURRENT USER
 
   	// create the form with validation
-  	this.form = this.formBuilder.group({
-      username: ['', Validators.required],
-      email: ['', Validators.compose([Validators.required, RadyModule.Validators.email])],
-      phone: ['', RadyModule.Validators.phone],
-      password: ['', Validators.required],
-      passwordConfirmation: ['', Validators.required]
-  	}, { validator: RadyModule.Validators.areEqual(['password', 'passwordConfirmation']) });
+    this.form = this.formBuilder.group({
+      username: [''],
+      email: [''],
+      phone: [''],
+      country: [''],
+      password: [''],
+      passwordConfirmation: ['']
+  	}, { validator: Validators.compose([
+      RadyModule.Validators.email('email', 'is not valid'),
+      RadyModule.Validators.phone('phone', 'country', 'is not valid'),
+      RadyModule.Validators.areEqual(['password', 'passwordConfirmation'], 'passwords are not equal'),
+      RadyModule.Validators.required(['username', 'email', 'password', 'passwordConfirmation'], 'is required')])
+    });
   }
 
   goToContactList(){
-    this.navCtrl.setRoot(ContactList);
+    var t: Tabs = this.navCtrl.parent;
+    // Select the 2nd tab
+    t.select(1);
   }
 
   save(){
     //TODO : SAVE USER LOCALY
     //TODO : SAVE USER ON SEVER
+    this.goToContactList();
+  }
+
+  get errors() {
+    return JSON.stringify(this.form.errors);
   }
 
 }
