396560735733ab8b3e72b37fa5b0f8c01dba4eda
Ornidon <thib.loiseau@gmail.com>
2016-11-15 14:15:06 +0100

    Merge branch 'edit-profile' into mobile-ui

--- app/src/pages/edit-profile/edit-profile.ts
+++ app/src/pages/edit-profile/edit-profile.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Validators, FormBuilder } from '@angular/forms';
+
+import { RadyModule } from '../../lib/validators';
+
+import { ContactList } from '../contact-list/contact-list';
 
 @Component({
-  selector: 'page-edit-profile',
   templateUrl: 'edit-profile.html'
 })
 export class EditProfile {
 
-  constructor(public navCtrl: NavController) {}
+  form;
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {}
 
   ionViewDidLoad() {
-    console.log('Hello EditProfile Page');
+    //TODO : GET CURRENT USER
+
+  	// create the form with validation
+  	this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, RadyModule.Validators.email])],
+      phone: ['', RadyModule.Validators.phone],
+      password: ['', Validators.required],
+      passwordConfirmation: ['', Validators.required]
+  	}, { validator: RadyModule.Validators.areEqual(['password', 'passwordConfirmation']) });
+  }
+
+  goToContactList(){
+    this.navCtrl.setRoot(ContactList);
+  }
+
+  save(){
+    //TODO : SAVE USER LOCALY
+    //TODO : SAVE USER ON SEVER
   }
 
 }
