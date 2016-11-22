c1912478e49eb0732124a090fb2978ad962fd68c
Damien Rochat <rochat.damien@gmail.com>
2016-11-22 15:23:12 +0100

    Change authorization token name

--- backend/rady/settings/__init__.py
+++ backend/rady/settings/__init__.py
@@ -107,7 +107,7 @@ JWT_AUTH = {
     "JWT_ALLOW_REFRESH": True,
     "JWT_EXPIRATION_DELTA": datetime.timedelta(days=30),
     "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=90),
-    "JWT_AUTH_HEADER_PREFIX": "TOKEN",
+    "JWT_AUTH_HEADER_PREFIX": "Bearer",
 }
 
 TEST_RUNNER = "rady.tests.RadyTestRunner"
