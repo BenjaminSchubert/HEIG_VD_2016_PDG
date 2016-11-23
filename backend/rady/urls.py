27727c47524f5921e518d5bad8fac8b39d07d13c
Damien Rochat <rochat.damien@gmail.com>
2016-11-23 10:24:15 +0100

    Fix duplicate url patterns

--- backend/rady/urls.py
+++ backend/rady/urls.py
@@ -10,12 +10,10 @@ from auth import urls as auth_urls
 apiv1_urls = [
     url(r"^users/", include("user.urls")),
     url(r"^docs/", include("rest_framework_docs.urls")),
-    url(r'^auth/', include(auth_urls)),
     url(r"^auth/", include(auth_urls)),
 ]
 
 urlpatterns = [
     url(r"^v1/", include(apiv1_urls)),
-    url(r'^admin/', admin.site.urls),
     url(r"^admin/", admin.site.urls),
 ]
