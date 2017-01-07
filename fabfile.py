79c3589fa0bf2ce17ae7e933cf3defd362c79d31
Benjamin Schubert <ben.c.schubert@gmail.com>
2017-01-07 10:47:56 +0100

    updated deploy script

--- fabfile.py
+++ fabfile.py
@@ -15,13 +15,15 @@ This script does not support automated configuration of uwsgi and nginx.
 import os
 
 # noinspection PyUnresolvedReferences
-from fabric.api import cd, hide, lcd, local, prefix, put, run, sudo
+from fabric.api import cd, hide, lcd, local, prefix, put, run, sudo, task, warn_only
 # noinspection PyUnresolvedReferences
-from fabric.colors import blue, green, red
+from fabric.colors import blue, green, red, cyan
 # noinspection PyUnresolvedReferences
 from fabric.state import output, env
 # noinspection PyUnresolvedReferences
 from fabric.context_managers import shell_env
+# noinspection PyUnresolvedReferences
+from fabric.utils import abort
 
 
 __author__ = "Benjamin Schubert <ben.c.schubert@gmail.com>"
@@ -50,77 +52,86 @@ LOCAL_FRONTEND = os.path.join(LOCAL_PATH, "frontend")
 LOCAL_VENV = os.path.join(LOCAL_PATH, os.path.expanduser("~/.virtualenvs/rady"))
 
 
-def start_section(section, color=blue):
-    print(color(section))
-    print(color("-" * 50))
+class Section:
+    def __init__(self, _section, color=blue):
+        self.section = _section
+        self.color = color
+
+    def __enter__(self):
+        print(self.color(self.section))
+        print(self.color("-" * 50))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(self.color("-") * 50)
+
+section = Section
 
 
-def stop_section(color=blue):
-    print(color("-") * 50)
-
-
+@task
 def lint_app():
     """
     Runs configured linters on the application
     """
-    with lcd(LOCAL_APP):
-        start_section("linting app")
-        local("npm run -s lint")
-        stop_section()
+    with lcd(LOCAL_APP), section("linting app"):
+        return local("npm run -s lint")
 
 
+@task
 def lint_backend():
     """
     Runs configured linters on the backend
     """
-    with lcd(LOCAL_BACKEND):
-        start_section("linting backend")
-        local("prospector")
-        stop_section()
+    with lcd(LOCAL_BACKEND), section("linting backend"):
+        return local("prospector")
 
 
+@task
 def lint_frontend():
     """
     Runs configured linters on the frontend
     """
-    with lcd(LOCAL_FRONTEND):
-        start_section("linting frontend")
-        local("npm run -s lint")
-        stop_section()
+    with lcd(LOCAL_FRONTEND), section("linting frontend"):
+        return local("npm run -s lint")
 
 
+@task
 def lint():
     """
     Runs all linters on the whole project
     """
-    start_section("linting project", green)
-    lint_app()
-    lint_frontend()
-    lint_backend()
-    stop_section(green)
+    with warn_only(), section("linting project", green):
+        tests = lint_app(), lint_frontend(), lint_backend(),
+
+    if any(not test.succeeded for test in tests):
+        abort("Linting failed")
 
 
+@task
 def test_backend():
     """
     Run tests on the backend
     """
-    with lcd(LOCAL_BACKEND):
-        start_section("making migrations")
-        local("FCM_SERVER_TOKEN='' python3 manage.py makemigrations")
-        start_section("testing backend")
-        local("FCM_SERVER_TOKEN='' python3 manage.py test")
-        stop_section()
+    with lcd(LOCAL_BACKEND), section("testing backend"):
+        with section("making migrations", cyan):
+            local("FCM_SERVER_TOKEN='' python3 manage.py makemigrations")
+
+        with section("running tests", cyan):
+            return local("FCM_SERVER_TOKEN='' python3 manage.py test")
 
 
+@task
 def test():
     """
     Runs all tests on the whole project
     """
-    start_section("testing project", green)
-    test_backend()
-    stop_section(green)
+    with warn_only(), section("testing project", green):
+        tests = test_backend(),
+
+    if any(not test.succeeded for test in tests):
+        abort("Tests failed")
 
 
+@task
 def check():
     """
     Checks that the application is in a working state
@@ -129,53 +140,50 @@ def check():
     test()
 
 
+@task
 def prepare_deploy():
     """
     Prepare files locally to deploy
     """
-    with lcd(LOCAL_FRONTEND):
-        with shell_env(NODE_ENV="production"):
-            local("npm run -s clean:dist")
-            local("npm run -s build")
+    with lcd(LOCAL_FRONTEND), shell_env(NODE_ENV="production"):
+        local("npm run -s clean:dist")
+        local("npm run -s build")
 
-    with lcd(LOCAL_BACKEND):
-        start_section("local cleanup")
+    with lcd(LOCAL_BACKEND), section("local cleanup"):
         local("""find . -type f -iname "*.pyc" -delete""")
         local("""find . -type d -empty -delete""")
         local("rm -rf ./backend/htmlcov")
         local("rm -f ./backend/.coverage")
-        stop_section()
 
 
+@task
 def copy_files():
     """
     Copy files to the remote server
     """
-    start_section("copying to server")
+    with section("copying to server"):
+        for path in [REMOTE_BACKEND, REMOTE_FRONTEND]:
+            sudo("mkdir -p " + path)
+            sudo("rm -rf " + os.path.join(path, "*"))
 
-    for path in [REMOTE_BACKEND, REMOTE_FRONTEND]:
-        sudo("mkdir -p " + path)
-        sudo("rm -rf " + os.path.join(path, "*"))
+        for path in [TEMPORARY_BACKEND_PATH, TEMPORARY_FRONTEND_PATH]:
+            run("mkdir -p " + path)
+            run("rm -rf " + os.path.join(path, "*"))
 
-    for path in [TEMPORARY_BACKEND_PATH, TEMPORARY_FRONTEND_PATH]:
-        run("mkdir -p " + path)
-        run("rm -rf " + os.path.join(path, "*"))
+        put("{}/*".format(LOCAL_BACKEND), TEMPORARY_BACKEND_PATH)
+        run("rm -f {}/rady.db".format(TEMPORARY_BACKEND_PATH))
+        sudo("cp -r {}/* {}".format(TEMPORARY_BACKEND_PATH, REMOTE_BACKEND))
 
-    put("{}/*".format(LOCAL_BACKEND), TEMPORARY_BACKEND_PATH)
-    run("rm -f {}/rady.db".format(TEMPORARY_BACKEND_PATH))
-    sudo("cp -r {}/* {}".format(TEMPORARY_BACKEND_PATH, REMOTE_BACKEND))
-
-    put("{}/dist/*".format(LOCAL_FRONTEND), TEMPORARY_FRONTEND_PATH, mode="0644")
-    sudo("cp -r {}/* {}".format(TEMPORARY_FRONTEND_PATH, REMOTE_FRONTEND))
-    stop_section()
+        put("{}/dist/*".format(LOCAL_FRONTEND), TEMPORARY_FRONTEND_PATH, mode="0644")
+        sudo("cp -r {}/* {}".format(TEMPORARY_FRONTEND_PATH, REMOTE_FRONTEND))
 
 
+@task
 def setup_env():
     """
     Setups the remote environment
     """
-    start_section("setup of environment")
-    with prefix("source {}/bin/activate".format(REMOTE_VENV)):
+    with prefix("source {}/bin/activate".format(REMOTE_VENV)), section("setup of environment"):
         with hide('output'):
             sudo("pip install -r {}/requirements.pip".format(REMOTE_BACKEND))
 
@@ -185,20 +193,20 @@ def setup_env():
 
     # reload uwsgi
     sudo("touch {}".format(UWSGI_WATCHER))
-    stop_section()
 
 
+@task
 def insecure_deploy():
     """
     Deploys the application without running any tests or checking anything
     """
-    start_section("deploy", green)
-    prepare_deploy()
-    copy_files()
-    setup_env()
-    stop_section(green)
+    with section("deploy"):
+        prepare_deploy()
+        copy_files()
+        setup_env()
 
 
+@task
 def deploy():
     """
     Deploys the complete application
